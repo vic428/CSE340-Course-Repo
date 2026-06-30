94def8110d04738fa11c003aa99b9edb4ebc3a3e
Archibong Victor <varchibong428@gmail.com>
2026-06-30 11:14:41 -0600

    Added a categories page

--- server.js
+++ server.js
@@ -44,6 +44,12 @@ app.get('/projects', (req, res) => {
     res.render('projects', { title });
 });
 
+app.get('/categories', (req, res) => {
+    const title = 'Service Project Categories';
+    res.render('categories', { title });
+
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running at http://127.0.0.1:${PORT}`);
     console.log(`Environment: ${NODE_ENV}`);
