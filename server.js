47df0d87a52cd26c913561cd7aabb7d2c0bee655
Archibong Victor <varchibong428@gmail.com>
2026-07-13 05:03:31 -0600

    db setup + node.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import { fileURLToPath } from 'url';
 import path from 'path';
+import { testConnection } from './src/models/db.js';
+import { getAllOrganizations } from './src/models/organizations.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -34,9 +36,11 @@ app.get('/', async (req, res) => {
     res.render('home', { title });
 });
 
-app.get('/organizations', (req, res) => {
+app.get('/organizations', async (req, res) => {
+    const organizations = await getAllOrganizations();
     const title = 'Our Partner Organizations';
-    res.render('organizations', { title });
+
+    res.render('organizations', { title, organizations });
 });
 
 app.get('/projects', (req, res) => {
@@ -54,8 +58,13 @@ app.get('/about-us', (req, res) => {
     res.render('about-us', {title});
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
+  try {
+    await testConnection();
     console.log(`Server is running at http://127.0.0.1:${PORT}`);
     console.log(`Environment: ${NODE_ENV}`);
+  } catch (error) {
+    console.error('Error connecting to the database:', error);
+  }
 
 });
